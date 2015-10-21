ec56c71ca4682242f9517139cb3b832b7ae60377
Asen Christov <asen.christov@gmail.com>
2015-10-21 14:27:45 +0200

    time pdfs

--- skylab/timeDep_model.py
+++ skylab/timeDep_model.py
@@ -1,7 +1,7 @@
 import ps_model
 PowerLawLLH = ps_model.PowerLawLLH
 import numpy as np
-
+from scipy.stats import norm
 
 class TimePDF(object):
     r""" time pdf, top level class, for given time returns value
@@ -11,8 +11,8 @@ class TimePDF(object):
         self.tstart=tstart
         self.tend=tend
         
-    def tPDFval(ev_time):
-        return self.__raise__()
+    def tPDFvals(self,ev_times):
+        return self.tPDFval_vect(self,ev_times)
 
 class TimePDFBinned(TimePDF):
     r""" the time pdf for the case it is binned, i.e. Fermi lightcurve or a Box.
@@ -21,21 +21,43 @@ class TimePDFBinned(TimePDF):
     and should be of the form [binstart,-1]
     """
     
-    def __init__(self,tstart,tend,time_bin_series):
+    def __init__(self,tstart,tend,time_bin_series,threshold=0):
         super(TimePDFBinned, self).__init__(tstart,tend)
-        
-        self.tPDF=np.recarray((len(time_bin_series)+1,), dtype=[('t', float), ('v', float)])
-        self.tPDF.t=np.asarray((tstart,)+zip(*time_bin_series)[0])
-        self.tPDF.v=np.asarray((0.,)+zip(*time_bin_series)[1][:-1]+(0.,))
+        self.TBS=time_bin_series
+        self.TBS_to_PDF(threshold)
+    
+    def TBS_to_PDF(self,threshold=0):
+        self.threshold=threshold
+        self.tPDF=np.recarray((len(self.TBS)+1,), dtype=[('t', float), ('v', float)])
+        self.tPDF.t=np.asarray((self.tstart,)+zip(*self.TBS)[0])
+        self.tPDF.v=np.asarray((0.,)+zip(*self.TBS)[1][:-1]+(0.,))
+        self.tPDF.v[self.tPDF.v < self.threshold] = 0.
         norm=0.
         for i in range(len(self.tPDF)-1):
             norm+=self.tPDF.v[i]*(self.tPDF.t[i+1]-self.tPDF.t[i])
         self.tPDF.v=self.tPDF.v/norm
         
-    def tPDFval(self,ev_time):
+    @staticmethod
+    @np.vectorize
+    def tPDFval_vect(self,ev_time):
         return self.tPDF.v[max(0,np.searchsorted(self.tPDF.t,ev_time)-1)]
         
-
+class TimePDFGauss(TimePDF):
+    r""" the time pdf for the case of a gaussuan shaped flare """
+    
+    def __init__(self,tstart,tend):
+        super(TimePDFGauss, self).__init__(tstart,tend)        
+        self.mean=(tend-tstart)/2.
+        self.sigma=self.mean/100.
+        
+    def setMeanAndSigma(self,mean,sigma):
+        self.mean=mean
+        self.sigma=sigma
+        
+    def tPDFvals(self,ev_times):
+        gfcn=norm(loc = self.mean, scale = self.sigma)
+        return gfcn.pdf(ev_times)
+        
 class TimeBoxLLH(PowerLawLLH):
     r""" Simplest time dep. search, a time PDF shaped as a box"""
     
