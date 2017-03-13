8cff2e2b424166e0dfad9a48dedb31fe21f5441e
christophra <christoph@raab.io>
2017-03-13 11:55:38 +0100

    manual merge master into timedep: doc/examples

--- doc/examples/data.py
+++ doc/examples/data.py
@@ -14,12 +14,15 @@ import numpy as np
 
 # skylab
 from skylab.psLLH import PointSourceLLH, MultiPointSourceLLH
-from skylab.ps_model import ClassicLLH, EnergyLLH
+from skylab.ps_model import UniformLLH, PowerLawLLH
+from skylab.ps_injector import PointSourceInjector
 
-log_mean = np.log(np.radians(2.5))
-log_sig = 0.5
-logE_res = 0.3
+mrs = np.radians(1.)
+mrs_min = np.radians(0.05)
+log_sig = 0.2
+logE_res = 0.1
 
+# fix seed to reproduce same results
 np.random.seed(1)
 
 def exp(N=100):
@@ -31,9 +34,11 @@ def exp(N=100):
 
     arr["ra"] = np.random.uniform(0., 2.*np.pi, N)
     arr["sinDec"] = np.random.uniform(-1., 1., N)
-    arr["sigma"] = np.random.lognormal(mean=log_mean, sigma=log_sig, size=N)
-    x = np.random.uniform(0., 1., size=N)
-    arr["logE"] = np.log10(1. - x) / (1. - g) + logE_res * np.random.normal(size=N)
+
+    E = np.log10(np.random.pareto(g, size=N) + 1)
+    arr["sigma"] = np.random.lognormal(mean=np.log((mrs - mrs_min) * np.exp(-np.log(10)*E) + mrs_min),
+                                       sigma=log_sig)
+    arr["logE"] = E + logE_res * np.random.normal(size=N)
 
     return arr
 
@@ -50,39 +55,47 @@ def MC(N=1000):
 
     arr["trueRa"] = np.random.uniform(0., 2.*np.pi, N)
     arr["trueDec"] = np.arcsin(np.random.uniform(-1., 1., N))
-    x = np.random.uniform(0., 1., N)
-    arr["trueE"] = (1. - x)**(1. / (1. - g))
-    arr["ow"] = arr["trueE"]
+    arr["trueE"] = np.random.pareto(g, size=N) + 1
+    arr["ow"] = arr["trueE"]**(g)
     arr["ow"] /= arr["ow"].sum()
 
     eta = np.random.uniform(0., 2.*np.pi, len(arr))
-    arr["sigma"] = np.random.lognormal(mean=log_mean, sigma=log_sig, size=N)
+    arr["sigma"] = np.random.lognormal(mean=np.log((mrs - mrs_min) * np.exp(-np.log(10)*np.log10(arr["trueE"])) + mrs_min),
+                                       sigma=log_sig)
     arr["ra"] = arr["trueRa"] + np.cos(eta) * arr["sigma"] / np.cos(arr["trueDec"])
     arr["sinDec"] = np.sin(arr["trueDec"] + np.sin(eta) * arr["sigma"])
     arr["logE"] = np.log10(arr["trueE"]) + logE_res * np.random.normal(size=len(arr))
 
     return arr
 
-def init(Nexp, NMC, energy=False, **kwargs):
-    arr_exp = exp(Nexp)
+def init(Nexp, NMC, energy=True, **kwargs):
+    Nsrc = kwargs.pop("Nsrc", 0)
+
+    arr_exp = exp(Nexp - Nsrc)
     arr_mc = MC(NMC)
 
+    if Nsrc > 0:
+        inj = PointSourceInjector(2, sinDec_bandwidth=1, seed=0)
+        inj.fill(0., arr_mc, 333.)
+
+        source = inj.sample(Nsrc, poisson=False).next()[1]
+
+        arr_exp = np.append(arr_exp, source)
+
     if energy:
-        llh_model = EnergyLLH(sinDec_bins=max(3, Nexp // 200),
-                              sinDec_range=[-1., 1.],
-                              twodim_bins=max(3, Nexp // 200),
-                              twodim_range=[[0.9 * min(arr_exp["logE"].min(),
-                                                       arr_mc["logE"].min()),
-                                             1.1 * max(arr_exp["logE"].max(),
-                                                       arr_exp["logE"].max())],
-                                             [-1., 1.]])
+        llh_model = PowerLawLLH(["logE"], min(50, Nexp // 50),
+                                range=[[0.9 * arr_mc["logE"].min(),
+                                        1.1 * arr_mc["logE"].max()]],
+                                sinDec_bins=min(50, Nexp // 50),
+                                sinDec_range=[-1., 1.],
+                                bounds=(0, 5))
     else:
-        llh_model = ClassicLLH(sinDec_bins=max(3, Nexp // 200),
+        llh_model = UniformLLH(sinDec_bins=max(3, Nexp // 200),
                                sinDec_range=[-1., 1.])
 
     llh = PointSourceLLH(arr_exp, arr_mc, 365., llh_model=llh_model,
                          mode="all", hemispheres=dict(Full=[-np.inf, np.inf]),
-                         nsource=Nexp / 100.,
+                         nsource=25, scramble=False,
                          nsource_bounds=(-Nexp / 2., Nexp / 2.)
                                         if not energy else (0., Nexp / 2.),
                          seed=np.random.randint(2**32),
@@ -94,7 +107,7 @@ def multi_init(n, Nexp, NMC, **kwargs):
     energy = kwargs.pop("energy", False)
 
     llh = MultiPointSourceLLH(hemispheres=dict(Full=[-np.inf, np.inf]),
-                              nsource=Nexp / 100.,
+                              nsource=25,
                               nsource_bounds=(-Nexp / 2., Nexp / 2.)
                                              if not energy else (0., Nexp / 2.),
                               seed=np.random.randint(2**32),
--- doc/examples/weighted_sensitivity.py
+++ doc/examples/weighted_sensitivity.py
@@ -1,35 +1,74 @@
 # -*-coding:utf8-*-
 
-import data
+from __future__ import print_function
 
+# SciPy
 from scipy.stats import chi2
 import healpy as hp
 import numpy as np
-import matplotlib.pyplot as plt
+from scipy.signal import convolve2d
 
+# skylab
 from skylab.ps_injector import PointSourceInjector
 from skylab.psLLH import MultiPointSourceLLH
+from skylab.ps_model import UniformLLH
+from skylab.utils import poisson_weight
+
+# local
+import utils
 
 if __name__=="__main__":
+    plt = utils.plotting(backend="pdf")
 
-    # init likelihood class
-    llh = data.multi_init(4, 1000, 100000, ncpu=1, energy=True)
-
-    N_MC = 1000
-    if isinstance(llh, MultiPointSourceLLH):
-        mc = dict([(key, data.MC(N_MC)) for key in llh._enum.iterkeys()])
-    else:
-        mc = data.MC(N_MC)
-
+    llh, mc = utils.startup()
     print(llh)
 
-    # init a injector class sampling events at a point source
-    inj = PointSourceInjector(2.)
+    N = 10
+    Gamma = np.linspace(1., 4., N)
 
-    # start calculation for dec = 0
-    result = llh.weighted_sensitivity(0., [0.5, 2.87e-7], [0.9, 0.5],
-                                      inj, mc,
-                                      n_bckg=1000,
-                                      n_iter=1000,
-                                      eps=5.e-2)
+    fig, ax = plt.subplots()
+
+    for energy in [True, False]:
+
+        l = "w/"
+        if not energy:
+            l += "o"
+
+            llh.llh_model = (UniformLLH(sinDec_bins=max(3, len(llh.exp) // 200),
+                                        sinDec_range=[-1., 1.]),
+                             mc)
+
+        props = dict()
+        sens = list()
+        disc = list()
+        for j, gamma in enumerate(Gamma):
+            # init a injector class sampling events at a point source
+            inj = PointSourceInjector(gamma, sinDec_bandwidth=1.)
+
+            # start calculation for dec = 0
+            result = llh.weighted_sensitivity(0., [0.5, 2.87e-7], [0.9, 0.5],
+                                              inj, mc,
+                                              n_bckg=10000,
+                                              n_iter=1000,
+                                              eps=1.e-2,
+                                              **props)
+
+            props["TSval"] = result["TSval"]
+
+            sens.append(result["mu"][0])
+            disc.append(result["mu"][1])
+
+        p = ax.plot(Gamma, disc, label="{0:s} energy".format(l))
+        ax.plot(Gamma, sens, color=p[0].get_color(), linestyle="dashed")
+
+    ax.set_xlabel(r"$\gamma$")
+    ax.set_ylabel(r"$\mu_\mathrm{inj}$")
+
+    ax.set_ylim(ymin=0.)
+
+    ax.legend(loc="best")
+
+    fig.savefig("figures/nevents.pdf")
+
+    plt.show()
 
